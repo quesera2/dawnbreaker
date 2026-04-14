8c28acee8ffe354d1580c846f7474376c7fdbad0
quesera2 <quesera2262@gmail.com>
2026-04-14 16:19:54 +0900

    リファクタ、インスタンスなしでFuriganaTranslateを使えるように修正

--- ios/Runner/Util/FuriganaTranslate.swift
+++ ios/Runner/Util/FuriganaTranslate.swift
@@ -3,18 +3,19 @@ import UIKit
 
 public class FuriganaTranslate: NSObject, FlutterPlugin {
     
+    private static let channelName = "que.sera.sera/furigana.translate"
+    private static let methodTranslateToFurigana = "translateToFurigana"
+    
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let channel = FlutterMethodChannel(
-            name: "que.sera.sera/furigana.translate",
-            binaryMessenger: registrar.messenger()
-        )
+        let channel = FlutterMethodChannel(name: channelName,
+                                           binaryMessenger: registrar.messenger())
         let instance = FuriganaTranslate()
         registrar.addMethodCallDelegate(instance, channel: channel)
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         switch call.method {
-        case "translateToFurigana":
+        case FuriganaTranslate.methodTranslateToFurigana:
             guard let text = call.arguments as? String else {
                 result(FlutterError(code: "INVALID_ARGUMENT", message: "String argument expected", details: nil))
                 return
@@ -26,21 +27,21 @@ public class FuriganaTranslate: NSObject, FlutterPlugin {
     }
     
     private func convert(_ input: String) -> String {
-        var output = ""
-        let range = CFRangeMake(0, input.utf16.count)
         let cfString = input as CFString
         let tokenizer = CFStringTokenizerCreate(
             kCFAllocatorDefault,
             cfString,
-            range,
+            CFRangeMake(0, input.utf16.count),
             kCFStringTokenizerUnitWordBoundary,
             Locale(identifier: "ja_JP") as CFLocale
         )
         
+        var output = ""
         var tokenType = CFStringTokenizerGoToTokenAtIndex(tokenizer, 0)
+        
         while tokenType.rawValue != 0 {
             let cfRange = CFStringTokenizerGetCurrentTokenRange(tokenizer)
-            // ローマ字読みを取得してひらがなに変換
+            // 直接ひらがなは取得できないため、一度ローマ字読みを取得してひらがなに変換する
             if let latin = CFStringTokenizerCopyCurrentTokenAttribute(
                 tokenizer,
                 kCFStringTokenizerAttributeLatinTranscription
@@ -48,16 +49,13 @@ public class FuriganaTranslate: NSObject, FlutterPlugin {
                 let mutable = NSMutableString(string: latin)
                 if CFStringTransform(mutable, nil, kCFStringTransformLatinHiragana, false) {
                     output.append(mutable as String)
-                } else if let sub = CFStringCreateWithSubstring(kCFAllocatorDefault, cfString, cfRange) {
-                    // 変換失敗時は元のトークンをそのまま使用
-                    output.append(sub as String)
+                } else if let original = CFStringCreateWithSubstring(kCFAllocatorDefault, cfString, cfRange) as? String {
+                    output.append(original)
                 }
-            } else {
-                // 英数字・記号など変換できないトークンは元のテキストをそのまま使用
-                if let sub = CFStringCreateWithSubstring(kCFAllocatorDefault, cfString, cfRange) {
-                    output.append(sub as String)
-                }
+            } else if let original = CFStringCreateWithSubstring(kCFAllocatorDefault, cfString, cfRange) as? String {
+                output.append(original)
             }
+            
             tokenType = CFStringTokenizerAdvanceToNextToken(tokenizer)
         }
         return output
