1a2f94a5907a4d10e5bc478eecf07b584c3ddf55
quesera2 <quesera2262@gmail.com>
2026-06-20 17:45:17 +0900

    devとprodで切り分けて起動できるようにもろもろ設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
+
 import 'package:dawnbreaker/app/app.dart';
 import 'package:dawnbreaker/core/notification/notification_permission_observer.dart';
 import 'package:dawnbreaker/core/notification/notification_service_impl.dart';
 import 'package:dawnbreaker/core/notification/task_notification_sync_notifier.dart';
 import 'package:dawnbreaker/data/preferences/shared_preferences_provider.dart';
-import 'package:dawnbreaker/firebase_options.dart';
+import 'package:dawnbreaker/firebase_options_dev.dart' as dev_options;
+import 'package:dawnbreaker/firebase_options_prod.dart' as prod_options;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
@@ -19,10 +22,16 @@ import 'package:timezone/timezone.dart' as tz;
 void main() async {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  const flavor = String.fromEnvironment('FLAVOR', defaultValue: 'dev');
+  final firebaseOptions = flavor == 'prod'
+      ? prod_options.DefaultFirebaseOptions.currentPlatform
+      : dev_options.DefaultFirebaseOptions.currentPlatform;
+  await Firebase.initializeApp(options: firebaseOptions);
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    unawaited(
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true),
+    );
     return true;
   };
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
