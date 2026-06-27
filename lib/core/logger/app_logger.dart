bea6d66cbd1f12fc707838946ac9f9ed5d2e7f2f
quesera2 <quesera2262@gmail.com>
2026-06-27 12:55:15 +0900

    loggerでテストがこけるのを修正

--- lib/core/logger/app_logger.dart
+++ lib/core/logger/app_logger.dart
@@ -1,17 +1,24 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 
-final logger = Logger(
-  output: kDebugMode
+final logger = _buildLogger();
+
+Logger _buildLogger() {
+  final isTest = Platform.environment.containsKey('FLUTTER_TEST');
+  final LogOutput output = isTest
+      ? ConsoleOutput()
+      : kDebugMode
       ? MultiOutput([ConsoleOutput(), _CrashlyticsOutput()])
-      : _CrashlyticsOutput(),
-  printer: kDebugMode
+      : _CrashlyticsOutput();
+  final LogPrinter printer = kDebugMode
       ? PrettyPrinter(methodCount: 2)
-      : SimplePrinter(printTime: true, colors: false),
-);
+      : SimplePrinter(printTime: true, colors: false);
+  return Logger(output: output, printer: printer);
+}
 
 final class _CrashlyticsOutput extends LogOutput {
   final _crashlytics = FirebaseCrashlytics.instance;
